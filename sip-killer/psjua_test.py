6413807847835f7b64dca6e61827ce7205c301ba
Alex45452 <youtube.no@mail.ru>
2022-08-31 20:29:05 +0300

    msg

--- sip-killer/psjua_test.py
+++ sip-killer/psjua_test.py
@@ -3,35 +3,6 @@ import sys
 import time
 
 
-# pjsua2 test function
-def pjsua2_test():
-    # Create and initialize the library
-    ep_cfg = pj.EpConfig()
-    ep = pj.Endpoint()
-    ep.libCreate()
-    ep.libInit(ep_cfg)
-
-    # Create SIP transport. Error handling sample is shown
-    sipTpConfig = pj.TransportConfig();
-    sipTpConfig.port = 5060;
-    ep.transportCreate(pj.PJSIP_TRANSPORT_UDP, sipTpConfig);
-    # Start the library
-    ep.libStart();
-
-    acfg = pj.AccountConfig();
-    acfg.idUri = "sip239888:@sip.novofon.com";
-    acfg.regConfig.registrarUri = "sip:sip.novofon.com";
-    cred = pj.AuthCredInfo("digest", "sip.novofon.com", "239888", 0, "ziduMGNT64");
-    acfg.sipConfig.authCreds.append( cred );
-    # Create the account
-    acc = Account();
-    acc.create(acfg);
-    # Here we don't have anything else to do..
-    time.sleep(10);
-
-    # Destroy the library
-    ep.libDestroy()
-
 
 LOG_LEVEL = 3
 CALL_STATUS = {
@@ -93,7 +64,54 @@ class Account(pj.Account):
         print("***OnRegState: " + prm.reason)
 
 class MyCall(pj.Call):
-    pass
+    def __init__(self,acc,call_id = pj.PJSUA_INVALID_ID):
+        super().__init__(acc, call_id)
+
+    def onCallState(prm):
+        pass
+
+    def onCallMediaState(prm):
+        pass
+    
+
+
+
+# pjsua2 test function
+def pjsua2_test():
+    # Create and initialize the library
+    ep_cfg = pj.EpConfig()
+    ep = pj.Endpoint()
+    ep.libCreate()
+    ep.libInit(ep_cfg)
+
+    # Create SIP transport. Error handling sample is shown
+    sipTpConfig = pj.TransportConfig();
+    sipTpConfig.port = 5060;
+    ep.transportCreate(pj.PJSIP_TRANSPORT_UDP, sipTpConfig);
+    # Start the library
+    ep.libStart();
+
+    acfg = pj.AccountConfig();
+    acfg.idUri = "sip239888:@sip.novofon.com";
+    acfg.regConfig.registrarUri = "sip:sip.novofon.com";
+    cred = pj.AuthCredInfo("digest", "sip.novofon.com", "239888", 0, "ziduMGNT64");
+    acfg.sipConfig.authCreds.append( cred );
+    # Create the account
+    acc = Account();
+    acc.create(acfg);
+    # Here we don't have anything else to do..
+    time.sleep(10);
+
+    # Destroy the library
+    ep.libDestroy()
+
+    main_call = MyCall(acc)
+    prm = pj.CallOpParam(True)
+    try:
+        main_call.make_call("sip:79150994289",prm)
+    except Exception as ex:
+        print(f"err occured : {ex}")
+
 
 
 def main():
