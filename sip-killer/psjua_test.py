076d4df0eed491b7af791515637194f30c018e17
Alex45452 <youtube.no@mail.ru>
2022-09-04 10:22:25 +0300

    msg

--- sip-killer/psjua_test.py
+++ sip-killer/psjua_test.py
@@ -1,5 +1,4 @@
 import pjsua2 as pj
-import sys
 import time
 
 # Subclass to extend the Account and get notifications etc.
@@ -11,10 +10,10 @@ class MyCall(pj.Call):
     def __init__(self,acc,call_id = pj.PJSUA_INVALID_ID):
         super().__init__(acc, call_id)
 
-    def onCallState(prm):
+    def onCallState(self, prm):
         pass
 
-    def onCallMediaState(prm):
+    def onCallMediaState(self, prm):
         pass
     
 
@@ -29,30 +28,29 @@ def pjsua2_test():
     ep.libInit(ep_cfg)
 
     # Create SIP transport. Error handling sample is shown
-    sipTpConfig = pj.TransportConfig();
-    sipTpConfig.port = 5060;
-    ep.transportCreate(pj.PJSIP_TRANSPORT_UDP, sipTpConfig);
+    sipTpConfig = pj.TransportConfig()
+    sipTpConfig.port = 5060
+    ep.transportCreate(pj.PJSIP_TRANSPORT_UDP, sipTpConfig)
     # Start the library
-    ep.libStart();
+    ep.libStart()
 
-    acfg = pj.AccountConfig();
-    acfg.idUri = "sip823662:@sip.comtube.com";
-    acfg.regConfig.registrarUri = "sip:sip.comtube.com";
-    cred = pj.AuthCredInfo("digest", "sip.comtube.com", "823622", 0, "cG24xD9f5qs5jZF");
-    acfg.sipConfig.authCreds.append( cred );
+
+    acfg = pj.AccountConfig()
+    acfg.idUri = "sip823662:@sip.comtube.com"
+    acfg.regConfig.registrarUri = "sip:sip.comtube.com"
+    acfg.regConfig.proxyUse = "sip.comtube.com"
+    cred = pj.AuthCredInfo("digest", "sip.comtube.com", "823622", 0, "cG24xD9f5qs5jZF")
+    acfg.sipConfig.authCreds.append( cred )
     # Create the account
-    acc = Account();
-    acc.create(acfg);
+    acc = Account()
+    acc.create(acfg)
     # Here we don't have anything else to do..
 
-    # Destroy the library
-    # pj.pj_thread_register()
-
-    main_call = MyCall(acc)
+    main_call = MyCall(acc, call_id=0)
     prm = pj.CallOpParam(True)
     try:
-        main_call.makeCall("sip:79150994289",prm)
-        time.sleep(10);
+        main_call.makeCall("sip:79150994289@sip.comtube.com",prm)
+        time.sleep(10)
     except Exception as ex:
         print(f"err occured : {ex}")
 
